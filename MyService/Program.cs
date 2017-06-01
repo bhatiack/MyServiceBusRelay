4eaf3b1b6ba34438e4c7f97141c96fa1f6aa79f1
charan.bhatia <charan.bhatia@nwea.org>
2017-06-01 12:47:51 -0700

    Add project files.

---
+++ MyService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
